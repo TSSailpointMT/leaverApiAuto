3dc8cc1e24fa2363c61a83f4016a1dbb76db98be
mmora27979 <mmora27979@hotmail.com>
2016-12-12 14:06:03 -0600

    TVR3 Removing entitlements for an identity

--- src/main/java/leaver/testcases/TVR3_RemoveEntitlementForIdentityTest.java
+++ src/main/java/leaver/testcases/TVR3_RemoveEntitlementForIdentityTest.java
@@ -10,6 +10,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
 
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
@@ -17,6 +18,8 @@ import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import org.xml.sax.SAXException;
+
+import com.hxtt.a.c;
 
 import leaver.datamodels.ProjectVariable;
 import leaver.models.manual.IdentityRequestModel;
@@ -28,6 +31,7 @@ import leaver.var.Constant;
 
 import sailpoint.api.TaskManager;
 import sailpoint.object.Identity;
+import sailpoint.object.IdentityEntitlement;
 import sailpoint.object.Link;
 import sailpoint.object.Rule;
 import sailpoint.object.TaskResult;
@@ -36,7 +40,11 @@ import sailpoint.object.WorkflowLaunch;
 import sailpoint.tools.GeneralException;
 import sailpoint.tools.Message;
 import sailpoint.tools.Util;
-
+import sailpoint.web.lcm.EntitlementsRequestBean.EntitlementIdentity;
+import workday.com.Integration_Document_FieldObjectIDType;
+import sailpoint.object.Application;
+import sailpoint.object.Attributes;
+import sailpoint.object.Entitlement;
 import sailpoint.object.Filter;
 import sailpoint.object.IdentitySnapshot;
 import sailpoint.object.QueryOptions;
@@ -44,6 +52,7 @@ import java.util.Iterator;
 
 public class TVR3_RemoveEntitlementForIdentityTest extends Initializer{
 	private String workflowName;
+	private Object link;
 
 	@Parameters({ "workflowName" })
 	@BeforeTest
@@ -55,21 +64,34 @@ public class TVR3_RemoveEntitlementForIdentityTest extends Initializer{
 	public void removeEntitlement() throws GeneralException, InterruptedException{
 		//Setting up the variables for this case...
 		HashMap<String,Object> launchVariables = new HashMap<String,Object>();
-
+		
 		// here we will give the data related to the Identity we will delete the Entitlements from
 		launchVariables.put(ProjectVariable.DELETE_ENTITLEMENTS.getName(), true);
 		//launchVariables.put(ProjectVariable., "AcctsPayableAccess");
-		launchVariables.put(ProjectVariable.IDENTITY_NAME.getName(), "Victor Fuzz");
-		
-		// this date will be used to compare with the creation of the snapshot
-		Date dateBeforeCreation = new Date();
+		launchVariables.put(ProjectVariable.IDENTITY_NAME.getName(), "Bob Fields");
 		
 		//configuring and launching the workflow
 		WorkflowModel workflowModel = new WorkflowModel(context);
 		WorkflowLaunch launch = workflowModel.launchExistingWorkflow(workflowName, "System", Constant.PROJECT_VARS, launchVariables, null);
 		
-		/*
-		 * pending validation
-		 * */
+		
+		// new QO to go through the entitlements 
+        QueryOptions qo = new QueryOptions();
+        qo.setOrderBy("created");
+        qo.setOrderAscending(false);
+        Filter f = Filter.eq("displayName", "Bob Fields");
+        qo.addFilter(f);
+		
+      //Verifying that the changes were made
+        List<IdentityEntitlement> listEntitlements= context.getObjects(IdentityEntitlement.class, qo);
+		
+        if (listEntitlements.isEmpty()){
+       		Assert.assertTrue(true);
+       	}
+       	else{
+       		Assert.assertTrue(false);
+       	}
+		
 	}
+
 }
